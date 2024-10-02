54015f651ba472ee8ddd4690c8679931e444608c
Stekjh <159554777+kjhkyiu@users.noreply.github.com>
2024-10-02 07:59:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é moda',
+    'conjunto de opiniões, gostos, assim como modos de agir, viver e sentir coletivos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da moda?',
+    'Paris'
+)
+
+criaCartao(
+    'Programação',
+    'Qual o nome do arconte geo?',
+    'Morax'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual o nome do mundo do genshin?',
+    'Teyvat'
+)
